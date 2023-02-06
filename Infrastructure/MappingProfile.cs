39367891a08dd084ada31f7ec7b155998217efc8
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-06 20:03:40 +0300

    add auto mapper

---
+++ Infrastructure/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Incoming;
+using Application.DTOs.Outgoing;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<DoctorIncomingDto, Doctor>();
+            CreateMap<Doctor, DoctorOutgoingDto>();
+
+            CreateMap<PatientIncomingDto, Patient>();
+            CreateMap<Patient, PatientOutgoingDto>();
+
+            CreateMap<ReceptionistIncomingDto, Receptionist>();
+            CreateMap<Receptionist, ReceptionistOutgoingDto>();
+        }
+    }
+}
