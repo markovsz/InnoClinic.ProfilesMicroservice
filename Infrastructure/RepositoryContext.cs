a62f4bc15d47fb7c37ffde8c509309166f8afe0c
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-06 03:07:33 +0300

    add db context

---
+++ Infrastructure/RepositoryContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Receptionist> Receptionists { get; set; }
+
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Doctor>().HasKey(e => e.Id);
+            builder.Entity<Doctor>().Property(e => e.Id).ValueGeneratedOnAdd();
+
+            builder.Entity<Patient>().HasKey(e => e.Id);
+            builder.Entity<Patient>().Property(e => e.Id).ValueGeneratedOnAdd();
+
+            builder.Entity<Receptionist>().HasKey(e => e.Id);
+            builder.Entity<Receptionist>().Property(e => e.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
