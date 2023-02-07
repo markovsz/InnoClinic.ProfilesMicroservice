c1e7e58af939c3532f2225c4d8ac3aa31b278894
markovsz <58207528+markovsz@users.noreply.github.com>
2023-02-07 16:25:40 +0300

    add controllers

---
+++ Api/Controllers/PatientsController.cs
@@ -0,0 +1,53 @@
+﻿using Application.DTOs.Incoming;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientsService _patientsService;
+
+        public PatientsController(IPatientsService patientsService)
+        {
+            _patientsService = patientsService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePatientAsync([FromBody] PatientIncomingDto incomingDto)
+        {
+            var patientId = await _patientsService.CreatePatientAsync(incomingDto);
+            return Created($"patient/{patientId}", patientId);
+        }
+
+        [HttpGet("patient/{patientId}")]
+        public async Task<IActionResult> GetPatientByIdAsync(Guid patientId)
+        {
+            var patient = await _patientsService.GetPatientByIdAsync(patientId);
+            return Ok(patient);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPatientsAsync()
+        {
+            var patient = await _patientsService.GetPatientsAsync();
+            return Ok(patient);
+        }
+
+        [HttpPut("patient/{patientId}")]
+        public async Task<IActionResult> UpdatePatientAsync(Guid patientId, [FromBody] PatientIncomingDto incomingDto)
+        {
+            await _patientsService.UpdatePatientAsync(patientId, incomingDto);
+            return NoContent();
+        }
+
+        [HttpDelete("patient/{patientId}")]
+        public async Task<IActionResult> DeletePatientAsync(Guid patientId)
+        {
+            await _patientsService.DeletePatientByIdAsync(patientId);
+            return NoContent();
+        }
+    }
+}
