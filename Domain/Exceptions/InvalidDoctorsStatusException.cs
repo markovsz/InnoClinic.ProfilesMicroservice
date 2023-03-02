6c14b02c5f1aaefe17524ef224ee47bbd87b65d9
markovsz <58207528+markovsz@users.noreply.github.com>
2023-03-02 16:08:15 +0300

    add custom InvalidDoctorsStatusException

---
+++ Domain/Exceptions/InvalidDoctorsStatusException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions
+{
+    public class InvalidDoctorsStatusException : Exception
+    {
+        public InvalidDoctorsStatusException()
+            : base($"doctor's status value is invalid")
+        { 
+        }
+    }
+}
