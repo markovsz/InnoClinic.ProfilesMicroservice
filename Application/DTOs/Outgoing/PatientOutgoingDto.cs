fc096c748f8c93aa662eedcff96b810e5515fc71
markovsz <58207528+markovsz@users.noreply.github.com>
2023-04-05 16:15:56 +0300

    add phone number property to patient

--- Application/DTOs/Outgoing/PatientOutgoingDto.cs
+++ Application/DTOs/Outgoing/PatientOutgoingDto.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
         public bool IsLinkedToAccount { get; set; }
         public DateTime BirthDate { get; set; }
         public string AccountId { get; set; }
